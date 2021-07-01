e55bb13011c6fbedb96e6ac4377a68b760dc0316
BranKein <brankein13@gm.gist.ac.kr>
2021-07-01 22:21:07 +0900

    Ch4 Counter 완성

---
+++ Ch4_Counter/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+// @ts-ignore
+import Styled from 'styled-components/native';
+import Counter from "~/Screens/Counter";
+
+const Container = Styled.View`
+  flex: 1;
+  background-color: #EEE;
+`;
+
+const App = () => {
+  return (
+    <Container>
+      <Counter title="This is a Counter App" initValue={5} />
+    </Container>
+  );
+};
+
+export default App;
